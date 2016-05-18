303b82d6ebeb2d1f4b4b1b09e03e71053b59544c
Sergey Kraykin <kraykin@gmail.com>
2016-05-18 17:26:41 +0200

    Add scraper code

--- scraper.rb
+++ scraper.rb
@@ -23,3 +23,41 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+require 'nokogiri'
+require 'open-uri'
+require 'scraperwiki'
+
+ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
+
+base_url = "http://pitchfork.com"
+page = Nokogiri::HTML(open(base_url + "/reviews/albums"))
+
+review_links = page.css(".album-link").map {|a| a['href']}
+
+reviews = review_links.map do |link|
+  review = Nokogiri::HTML(open(base_url + link))
+  
+  artist = review.css(".artist-list a").text
+  album = review.css(".review-title").text
+  label = review.css(".label-list li").text
+  year = review.css(".year span+ span").text
+  reviewer = review.css(".display-name").text
+  review_date = Date.parse(review.css(".pub-date")[0]['title'])
+  score = review.css(".score").text
+  
+  {
+    artist: artist,
+    album: album,
+    label: label,
+    year: year,
+    reviewer: reviewer,
+    review_date: review_date,
+    score: score
+  }
+  
+end
+
+reviews.each do |review|
+  ScraperWiki.save_sqlite([:artist, :album], review)
+end
