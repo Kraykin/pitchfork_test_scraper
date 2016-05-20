da978ca1698842e3c410b05b44986534410e8688
Sergey Kraykin <kraykin@gmail.com>
2016-05-20 20:28:18 +0200

    Refactoring

--- scraper.rb
+++ scraper.rb
@@ -42,37 +42,36 @@ db.execute 'CREATE  TABLE IF NOT EXISTS Data
       score TEXT
     )'
 
-base_url = "http://pitchfork.com"
+base_url = 'http://pitchfork.com'
 pages_to_load = 10
 
-pages_to_load.times do |n| 
-  pages = Nokogiri::HTML(open(base_url + "/reviews/albums/?page=" + n.to_s))
-  review_links = pages.css(".album-link").map {|a| a['href']}
-  
-  reviews = review_links.map do |link|
-    review = Nokogiri::HTML(open(base_url + link))
+pages_to_load.times do |n|
+  page = Nokogiri::HTML(open(base_url + '/reviews/albums/?page=' + n.to_s))
+  links_on_reviews = page.css('.album-link').map { |a| a['href'] }
 
-    artist = review.css(".artist-list a").text
-    album = review.css(".review-title").text
-    label = review.css(".label-list li").text
-    year = review.css(".year span+ span").text
-    reviewer = review.css(".display-name").text
-    review_date = Date.parse(review.css(".pub-date")[0]['title']).to_s
-    score = review.css(".score").text
+  links_on_reviews.map do |link|
+    review_page = Nokogiri::HTML(open(base_url + link))
+
+    artist = review_page.css('.artist-list li').text
+    album = review_page.css('.review-title').text
+    label = review_page.css('.label-list li').text
+    year = review_page.css('.year span+ span').text
+    reviewer = review_page.css('.display-name').text
+    review_date = Date.parse(review_page.css('.pub-date')[0]['title']).to_s
+    score = review_page.css('.score').text
 
     db.execute 'INSERT INTO Data
-    (
-      artist,
-      album,
-      label,
-      year,
-      reviewer,
-      review_date,
-      score
-    )
-    VALUES ( ?, ?, ?, ?, ?, ?, ? )', [artist, album, label, year, reviewer, review_date, score]
+      (
+        artist,
+        album,
+        label,
+        year,
+        reviewer,
+        review_date,
+        score
+      )
+      VALUES ( ?, ?, ?, ?, ?, ?, ? )', [artist, album, label, year, reviewer, review_date, score]
 
     sleep(3.0 + rand * 2)
   end
-
 end
